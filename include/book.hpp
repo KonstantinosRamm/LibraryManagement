6a657f111ca760658f35d7b2fcdc9dda7ad7618d
KonstantinosRamm <ramkwnstantinos@gmail.com>
2025-02-11 19:00:29 +0200

    Managed to make Cmake configuration and made the basic hierarchy of the project

---
+++ include/book.hpp
@@ -0,0 +1,13 @@
+#include <string>
+
+
+typedef struct book
+{
+    std::string Title;
+    std::string Author;
+    std::string Publisher;
+    std::string Genre;
+    int year;
+    int ISBN;
+
+}book;
