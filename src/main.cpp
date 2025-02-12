0db8aac9f699e6e724f74ce1c80516129b1199f0
KonstantinosRamm <ramkwnstantinos@gmail.com>
2025-02-12 16:23:08 +0200

    nothing important .just made .gitignore and tested CMake

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,8 @@
-//#include "book.hpp"
 #include "book.hpp"
 #include <iostream>
 
 
 int main(void)
 {
-    book test;
-    std::cout << test.Author;
-    std::cout << test.Title;
     return 0;
 }
